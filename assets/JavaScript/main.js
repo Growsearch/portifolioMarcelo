d803aa00f987cbbb7595078d938db66db48eca45
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-08-01 17:06:21 -0300

    finalizando skills e dando inicio a qualification

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -31,8 +31,36 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // =============ACCORDION LIST========== //
 
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+
+    for(i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if(itemClass === 'skills__content  skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+};
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
+
+
 // ============ QUALIFICATION TABS =======  // 
 
 // ========== SERVICES MODAL =========== // 
+
+// =========== portfolio swipper ======== //
+
+// =========== testional =============== // 
+
+// ============ scroll sections active link ==== // 
+
+// ========== change background header ======= // 
+
 
 
