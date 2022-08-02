df0a068056a456fbdb26ce786f287a82d02585d6
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-08-02 16:00:24 -0300

    terminando  a parte de qualificacoes
    terminando qualificações e iniciando serviços

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -43,7 +43,7 @@ function toggleSkills() {
     if(itemClass === 'skills__content  skills__close') {
         this.parentNode.className = 'skills__content skills__open';
     }
-};
+}
 
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills);
@@ -52,7 +52,28 @@ skillsHeader.forEach((el) => {
 
 // ============ QUALIFICATION TABS =======  // 
 
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+});  
+
 // ========== SERVICES MODAL =========== // 
+
+
 
 // =========== portfolio swipper ======== //
 
