4f77611a99ce9e419195bd26e2605678b46b7969
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-08-03 17:45:22 -0300

    completando a parte de servicos
    terminando servicos e iniciando portfolio

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -71,11 +71,45 @@ tabs.forEach(tab => {
     })
 });  
 
-// ========== SERVICES MODAL =========== // 
+// ========== SERVICES MODAL =========== //
+
+const modalViews = document.querySelectorAll('.services__modal'),
+modalBtns = document.querySelectorAll('.services__button'),
+modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
 
 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+       modalViews.forEach((modalView) => {
+        modalView.classList.remove('active-modal');
+       }) 
+    })
+})
 
 // =========== portfolio swipper ======== //
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 // =========== testional =============== // 
 
