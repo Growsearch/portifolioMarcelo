530fff80bb14dee04ce0b498539faf9e356e34ed
Grow Search <93290454+Growsearch@users.noreply.github.com>
2022-08-05 17:10:02 -0300

    completando o portfolio
    completando toda parte de testemunhas , contate-me e footer... so falta o dark theme

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -97,7 +97,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 // =========== portfolio swipper ======== //
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -113,9 +113,60 @@ let swiper = new Swiper('.portfolio__container', {
 
 // =========== testional =============== // 
 
-// ============ scroll sections active link ==== // 
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48, 
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoits: {
+        568:{
+            slidesPerview: 2,
+        }
+    }
+});
+
+// ====== active link ==== // 
+//SCROLL SECTIONS ACTIVE LINK 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 // ========== change background header ======= // 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the
+    // header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
+
+// ====== show scroll up ======== // 
+function scrollUp(){
+    const scrollup = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollup.classList.add('show-scroll'); else scrollup.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
